4c8b2ea487dcabe40d76db38fe2d566202ff2179
murasame <oogiriminister@gmail.com>
2023-06-21 13:47:13 +0900

    fix: add account money deadlock

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTranferTX(t *testing.T) {
+func TestTranferTXDeadlock(t *testing.T) {
 	store := NweStore(testDB)
 
 	account1 := createRandomAccount(t)
@@ -16,89 +16,37 @@ func TestTranferTX(t *testing.T) {
 
 	// 並列で複数のトランザクションを行う
 
-	n := 5
+	n := 10
 	amount := int64(10)
 
 	errs := make(chan error)
-	results := make(chan TransferTxResult)
 
 	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+
 		go func() {
-			result, err := store.TransferTx(context.Background(), TransferTxParams{
-				FromAccountID: account1.ID,
-				ToAccountID:   account2.ID,
+
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
 				Amount:        amount,
 			})
 			errs <- err
-			results <- result
 		}()
 	}
 
 	// 結果の検証
-	existed := make(map[int]bool)
-
 	for i := 0; i < n; i++ {
 		err := <-errs
 		require.NoError(t, err)
-
-		result := <-results
-		require.NotEmpty(t, result)
-		// Transferの確認
-		transfer := result.Transfer
-		require.NotEmpty(t, transfer)
-		require.Equal(t, account1.ID, transfer.FromAccountID)
-		require.Equal(t, account2.ID, transfer.ToAccountID)
-		require.Equal(t, amount, transfer.Amount)
-		require.NotZero(t, transfer.ID)
-		require.NotZero(t, transfer.CreatedAt)
-
-		_, err = store.GetTransfer(context.Background(), transfer.ID)
-		require.NoError(t, err)
-		// エントリーの確認
-		fromEntry := result.FromEntry
-		require.NotEmpty(t, fromEntry)
-		require.Equal(t, account1.ID, fromEntry.AccountID)
-		require.Equal(t, -amount, fromEntry.Amount)
-		require.NotZero(t, fromEntry.ID)
-		require.NotZero(t, fromEntry.CreatedAt)
-
-		_, err = store.GetEntry(context.Background(), fromEntry.ID)
-		require.NoError(t, err)
-		// エントリーの確認
-		ToEntry := result.ToEntry
-		require.NotEmpty(t, ToEntry)
-		require.Equal(t, account2.ID, ToEntry.AccountID)
-		require.Equal(t, amount, ToEntry.Amount)
-		require.NotZero(t, ToEntry.ID)
-		require.NotZero(t, ToEntry.CreatedAt)
-
-		_, err = store.GetEntry(context.Background(), ToEntry.ID)
-		require.NoError(t, err)
-		//TODO:残高の確認
-		// アカウントのチェック
-		fromAccount := result.FromAccount
-		require.NotEmpty(t, fromAccount)
-		require.Equal(t, account1.ID, fromAccount.ID)
-
-		toAccount := result.ToAccount
-		require.NotEmpty(t, toAccount)
-		require.Equal(t, account2.ID, toAccount.ID)
-
-		// アカウントの残高確認
-
-		fmt.Println(">> tx: ", fromAccount.Balance, toAccount.Balance)
-		diff1 := account1.Balance - fromAccount.Balance
-		diff2 := toAccount.Balance - account2.Balance
-
-		require.Equal(t, diff1, diff2)
-		require.True(t, diff1 > 0)
-		require.True(t, diff1%amount == 0)
-
-		k := int(diff1 / amount)
-		require.True(t, k >= 1 && k <= n)
-		require.NotContains(t, existed, k)
-		existed[k] = true
 	}
+
 	// 最終的な残高の確認
 	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
 	require.NoError(t, err)
@@ -106,7 +54,7 @@ func TestTranferTX(t *testing.T) {
 	require.NoError(t, err)
 
 	fmt.Println(">> after: ", updatedAccount1.Balance, updatedAccount2.Balance)
-	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
-	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
 
 }
